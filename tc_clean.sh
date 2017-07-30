f0472f588edd24f9ae1ca408f48e15e006fa7e9a
haim <haim@alcide.io>
2017-07-30 08:57:07 +0300

    clean filter on nics

---
+++ tc_clean.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+this_file=$(basename $0)
+this_dir=$(dirname $0)
+this_dir=$(cd ${this_dir};pwd)
+TC=${this_dir}/tc
+
+
+clean_nic(){
+    local nic=${1}
+    ifconfig ${nic} 1>/dev/null 2>&1 
+    if [ $? -ne 0 ];then
+        echo "ERROR nic \"${nic}\" not exist"
+        return 1
+    fi
+    sudo -E ${TC} filter del dev ${nic} ingress
+    sudo -E ${TC} filter del dev ${nic} egress
+    sudo -E ${TC} qdisc del dev ${nic} clsact
+    return 0
+}
+
+main()
+{
+    local nic=$1
+    if [ "${nic}"x != ""x ];then
+        clean_nic ${nic}
+        return $?
+    fi
+
+    for i in $(ifconfig | grep -o -e ^"[a-zA-Z0-9_\-]*");do 
+        echo "INFO clean filter from ${i}" 
+        clean_nic ${i}
+    done
+    return 0
+}
+
+main $@
+exit $?
